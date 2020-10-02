6fa93d5a85e8745d298ef0dec7d018f07de7928f
Shilpa <u1323117@utah.edu>
2020-10-02 13:33:25 -0600

    Added the support to detect tap and swipe

---
+++ Assets/Scripts/Swipe/SwipeDetector.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeDetector : MonoBehaviour
+{
+    private Vector2 fingerDownPosition;
+    private Vector2 fingerUpPosition;
+
+    [SerializeField]
+    private float minDistanceForSwipe = 20f;
+    public Text test;
+
+    SwipeDirection swipeDirection;
+    SwipeDirection nextSwipeDirection;
+    int val = 1;
+    void start()
+    {
+        nextSwipeDirection = SwipeDirection.Up;
+    }
+
+    private void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                fingerUpPosition = touch.position;
+                fingerDownPosition = touch.position;
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                fingerDownPosition = touch.position;
+                DetectSwipeDirection();
+
+                if (swipeDirection == nextSwipeDirection)
+                {
+                    test.text = "SWIPE " + Enum.GetName(typeof(SwipeDirection), val) + " !";
+                    print("Swipe detected...");
+                    nextSwipeDirection = (SwipeDirection)val;
+                    val++;
+                    if (val > 3)
+                    {
+                        val = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    private void DetectSwipeDirection()
+    {
+        if (SwipeDistanceCheckMet())
+        {
+            if (IsVerticalSwipe())
+            {
+                swipeDirection = fingerDownPosition.y - fingerUpPosition.y > 0 ? SwipeDirection.Up : SwipeDirection.Down;
+
+            }
+            else
+            {
+                swipeDirection = fingerDownPosition.x - fingerUpPosition.x > 0 ? SwipeDirection.Right : SwipeDirection.Left;
+
+            }
+            fingerUpPosition = fingerDownPosition;
+
+        }
+
+
+    }
+
+    private bool IsVerticalSwipe()
+    {
+        return VerticalMovementDistance() > HorizontalMovementDistance();
+    }
+
+    private bool SwipeDistanceCheckMet()
+    {
+        return VerticalMovementDistance() > minDistanceForSwipe || HorizontalMovementDistance() > minDistanceForSwipe;
+    }
+
+    private float VerticalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.y - fingerUpPosition.y);
+    }
+
+    private float HorizontalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.x - fingerUpPosition.x);
+    }
+}
+    
+public enum SwipeDirection
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
---
+++ Assets/Scripts/Tap/TouchDetector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchDetector : MonoBehaviour
+{
+    public GameObject particle;
+    public Text test;
+    int count = 5;
+    void start()
+    {
+        test.text = "Tap on the screen 5 times exactly!";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+
+            if(count == 0)
+            {
+                test.text = "DO NOT TAP ANYMORE!!!!";
+                count = 5;
+            }
+            else
+            {
+                test.text = "Tap " + count + " more times!";
+                count--;
+            }
+            print("Tap detected...");
+            
+            
+        }
+
+
+    }
+
+
+
+
+
+
+}
