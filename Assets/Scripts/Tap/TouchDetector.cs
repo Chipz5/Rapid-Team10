9d41253c535d254327d024cb560ae260d89f8ff5
Shilpa <u1323117@utah.edu>
2020-10-04 17:51:18 -0600

    implemented return to home screen from tap screen

--- Assets/Scripts/Tap/TouchDetector.cs
+++ Assets/Scripts/Tap/TouchDetector.cs
@@ -11,12 +11,12 @@ public class TouchDetector : MonoBehaviour
 {
     public Text test;
     int count = 5;
-    public GameObject closeSprite;
+    public GameObject button;
     void Awake()
     {
         test.text = "Tap on the screen 5 times exactly!";
-        closeSprite = GameObject.Find("CloseOption");
-        closeSprite.SetActive(true);
+        button = GameObject.Find("Button");
+        button.SetActive(false);
 
     }
     // Update is called once per frame
@@ -24,56 +24,28 @@ public class TouchDetector : MonoBehaviour
     {
         if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
-            Vector3 tapPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            Vector2 tapPos2D = new Vector2(tapPos.x, tapPos.y);
-            RaycastHit2D hit = Physics2D.Raycast(tapPos2D, Vector2.zero);
-            //Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            print(Input.GetTouch(0).position);
-            print(closeSprite.transform.position);
-            
-            print(closeSprite.transform.name);
-            print(hit);
-            //RaycastHit raycastHit;
-            //print(Physics.Raycast(raycast, out raycastHit));
-            if (hit.collider != null)
+            if( count == 6)
             {
-                print("Something was hit!");
+                test.text = "You tapped too many times.... START AGAIN!!!!";
+                count--;
             }
-            /*if (Physics.Raycast(raycast, out raycastHit))
-            {
-                print(raycastHit.transform.name);
-                print("Something Hit");
-                if (raycastHit.transform.name == "CloseOption")
-                {
-                    print("Close clicked");
-                }
-
-            }*/
-            /* if(count == 0)
+            else if (count == 0)
              {
                  test.text = "DO NOT TAP ANYMORE!!!!";
-                 count = 5;
-                 closeSprite.SetActive(true);
-                 Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                 RaycastHit raycastHit;
-                 if (Physics.Raycast(raycast, out raycastHit))
-                 {
-                     print("Something Hit");
-                     if (raycastHit.collider.name == "CloseOption")
-                     {
-                         print("Close clicked");
-                     }
+                 count = 6;
+                button.SetActive(true);
 
-                 }
-                 //SceneManager.LoadScene(sceneName: "SwipeScene");
-             }
+            }
              else
              {
-                 //closeSprite.SetActive(false);
-                 test.text = "Tap " + count + " more times!";
-                 count--;
-             }
-             print("Tap detected...");*/
+                button.SetActive(false);
+                test.text = "Tap " + count + " more times!";
+                
+                count--;
+                
+                //button.SetActive(false);
+            }
+             print("Tap detected...");
 
 
         }
@@ -81,7 +53,10 @@ public class TouchDetector : MonoBehaviour
 
     }
 
-
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(sceneName: "Movement");
+    }
 
 
 
